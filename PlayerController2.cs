eb0d30a5d12743328805e60b40b787e0ca32d751
Andrew McGowan <31895448+AndrewMcg2309@users.noreply.github.com>
2018-05-02 22:29:35 +0100

    one

---
+++ PlayerController2.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour 
+{
+	private CharacterController characterController;
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	[SerializeField]
+	private float moveSpeed = 500;
+
+	private void Awake()
+	{
+		characterController = GetComponent<CharacterController> ();
+	}
+
+	private void Update ()
+	{
+		var horizontal = Input.GetAxis ("Horizontal");
+		var vertical = Input.GetAxis ("Vertical");
+
+		var movement = new Vector3 (horizontal, 0, vertical);
+
+		characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire();
+		}
+	}
+
+
+	void Fire()
+	{
+		// create the bullet
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// velocity to bullets
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 4;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);        
+	}
+
+
+
+}
---
+++ ZombieAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombiespwaning: MonoBehaviour 
+{
+	public GameObject  target; //the enemy's target
+
+	[SerializeField]
+
+	private float moveSpeedZombie = 200; //move speed
+	var rotationSpeed = 50; //speed of turning
+
+	var myTransform : Transform; // transform data of this enemy
+
+	function Awake()
+	{
+		myTransform = transform; //cache data for easy access
+	}
+
+	function Start()
+	{
+		target = GameObject.FindWithTag("Player").transform; //target the player
+
+	}
+
+	function Update () {
+		//rotate to look at the player
+		myTransform.rotation = Zombie.Slerp(myTransform.rotation,
+			Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+
+		//move towards the player
+		myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+
+	}
+
+
+
+
+
+}
